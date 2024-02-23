44bc066b5bbcb460032989fcf7acaa28dc7283ba
tim509314 <89288349+tim509314@users.noreply.github.com>
2024-02-24 03:25:15 +0800

    Add files via upload

--- Assignment1/Exercise4.py
+++ Assignment1/Exercise4.py
@@ -1,32 +1,29 @@
 import random
 
-min = 1
-max = 20
+min_range = 1
+max_range = 20
 
-intRandNum = random.randint(min, max)
-intUserInput = 0
-time = 0
-""" print("The answer is: ", intRandNum) """
+secret_number = random.randint(min_range, max_range)
+user_input = 0
+attempts = 0
 
-def guessNum():
-    global intUserInput
+def get_user_guess():
     while True:
         try:
-            intUserInput = int(input("Please type a number: "))
-            break
+            guess = int(input(f"Please type a number between {min_range} and {max_range}: "))
+            if min_range <= guess <= max_range:
+                return guess
+            else:
+                print("Invalid input. Please enter a number within the specified range.")
         except ValueError:
-            print("Oops!  That was no valid number.  Try again...")
+            print("Invalid input. Please enter a valid number.")
 
+while user_input != secret_number:
+    user_input = get_user_guess()
+    if user_input < secret_number:
+        print("The number is smaller than the answer.")
+    elif user_input > secret_number:
+        print("The number is larger than the answer.")
+    attempts += 1
 
-while intUserInput != intRandNum:
-    guessNum()
-    if min <= intUserInput <= max:
-        if intUserInput < intRandNum:
-            print("The number is smaller than answer.")
-        elif intUserInput > intRandNum:
-            print("The number is larger than answer.")
-        time += 1
-    else:
-        print("Please type a number between {min} and {max}.")
-    
-print("Good job! You guessed my number in {time} guesses!")
+print(f"Good job! You guessed my number in {attempts} guesses!")
